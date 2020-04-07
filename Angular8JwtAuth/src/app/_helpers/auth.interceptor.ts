3033ba1da6b845f69cb35b2e4a079a0075061c5c
tetsuo23 <carretmo@gmail.com>
2020-04-07 16:00:45 +0200

    ...

--- Angular8JwtAuth/src/app/_helpers/auth.interceptor.ts
+++ Angular8JwtAuth/src/app/_helpers/auth.interceptor.ts
@@ -4,7 +4,7 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 
 import { TokenStorageService } from '../_services/token-storage.service';
 
-const TOKEN_HEADER_KEY = 'Authorization';
+const TOKEN_HEADER_KEY = 'x-access-token';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -14,11 +14,15 @@ export class AuthInterceptor implements HttpInterceptor {
         let authReq = req;
         const token = this.token.getToken();
         if (token != null) {
-            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
         }
         return next.handle(authReq);
     }
 }
+
+
+
+
 
 export const authInterceptorProviders = [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
